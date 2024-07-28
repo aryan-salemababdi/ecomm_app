fa7b4c90fb11417ed12d4011a2fad1bac3000665
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-07-28 18:50:10 +0330

    add marketing in containerApp and set some config

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import MarketingApp from './components/Marketing/MarketingApp';
+
+export default () => {
+    return (
+        <>
+            <h1>سلام!</h1>
+            <MarketingApp />
+        </>
+    )
+}
