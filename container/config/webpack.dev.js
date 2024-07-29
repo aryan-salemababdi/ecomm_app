ce8b81c8d492cc5a3e9c6bc10950f5044acc9e52
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2024-07-29 15:10:03 +0330

    set production webpack configuration

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,7 @@
-const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const commonConfig = require('./webpack.common')
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 const devConfig = {
     mode: 'development',
